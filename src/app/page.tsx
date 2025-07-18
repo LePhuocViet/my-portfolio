e2f39627b0a2bc100ad2cbe0d1da7c282a5c83e4
LePhuocViet <139203809+LePhuocViet@users.noreply.github.com>
2025-07-19 00:22:19 +0700

    feat/menu-and-page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,86 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { Info } from "@/components/_pages/info/Info";
+import "./globals.css";
+import { Box } from "@mui/material";
+import { Menu } from "@/components/_pages/menu/Menu";
+import { Footer } from "@/components/_pages/footer/Footer";
+import { HomePage } from "@/components/_pages/home/Home";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        backgroundColor: "var(--lpv-core-black-1)",
+        paddingX: "20%",
+        paddingTop: 2,
+        boxSizing: "border-box",
+      }}
+    >
+      <Box
+        sx={{
+          width: "20%",
+          height: "100vh",
+          position: "fixed",
+          left: 0,
+          top: 0,
+          padding: 2,
+          boxSizing: "border-box",
+        }}
+      >
+        <Box
+          sx={{
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+            boxShadow: 10,
+            borderRadius: 2,
+            backgroundColor: "#1e1e1e",
+          }}
+        >
+          <Box sx={{ flex: 1 }}>
+            <Info />
+          </Box>
+          <Box sx={{ flex: 3 }}>
+            <Menu />
+          </Box>
+          <Box sx={{ flex: 1 }}>
+            <Footer />
+          </Box>
+        </Box>
+      </Box>
+
+      <Box
+        sx={{
+          width: "20%",
+          height: "100vh",
+          position: "fixed",
+          right: 0,
+          top: 0,
+          padding: 2,
+          boxSizing: "border-box",
+        }}
+      >
+        <Box
+          sx={{
+            height: "100%",
+            backgroundColor: "gray",
+            borderRadius: 2,
+            boxShadow: 10,
+          }}
+        >
+          Right
+        </Box>
+      </Box>
+
+      <Box
+        sx={{
+          width: "100%",
+          pb: 2,
+          borderRadius: 2,
+        }}
+      >
+        <HomePage />
+      </Box>
+    </Box>
+  );
 }
---
+++ src/components/commons/button-menu/ButtonMenu.tsx
@@ -0,0 +1,78 @@
+import { Box, Button, Typography } from "@mui/material";
+import React from "react";
+
+export interface ButtonMenuProps {
+  icon?: React.ReactNode;
+  label?: string;
+  isSelected?: boolean;
+  onClick?: () => void;
+}
+
+export const ButtonMenu = ({
+  icon,
+  label,
+  onClick,
+  isSelected,
+}: ButtonMenuProps) => {
+  console.log("ButtonMenu rendered", { icon, label, isSelected });
+  return (
+    <Box
+      sx={{
+        backgroundColor: isSelected ? "var(--lpv-core-black-1)" : "none",
+        ":hover": {
+          backgroundColor: "var(--lpv-core-black-1)",
+        },
+      }}
+    >
+      <Button
+        onClick={onClick}
+        variant="outlined"
+        sx={{
+          width: "100%",
+          color: "var(--lpv-core-white-0)",
+          textTransform: "none",
+          display: "flex",
+          justifyContent: "start",
+          outline: "none",
+          border: "none",
+          "&:hover": {
+            backgroundColor: "transparent",
+            boxShadow: "none",
+            border: "none",
+          },
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "end",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              pl: 0.5,
+              pr: 0.5,
+              pt: 0.8,
+              pb: 0.8,
+            }}
+          >
+            {icon}
+          </Box>
+          <Typography
+            fontSize="0.9rem"
+            fontWeight={700}
+            sx={{
+              alignItems: "center",
+              pl: 2,
+            }}
+          >
+            {label}
+          </Typography>
+        </Box>
+      </Button>
+    </Box>
+  );
+};
